bdce79cd2b3b07237edd30bbc118c7276b1cbe72
Steve <tetraman4arms@gmail.com>
2018-01-14 03:07:30 -0400

    final touches

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,4 +5,52 @@ $(document).ready(function(){
     nextArrow: '<div class="custom-next"><i class="fas fa-chevron-right fa-lg"></i></div>'
   });
   $('.destination').equalHeights();
+  // $('.overview-elem .btn').click(function() {
+  //   var image = $(this).data("data-img");
+  //   console.log(image);
+  //   $('.change-overview').attr("src", image);
+  // })
+  $(".overview-elem .btn").on('click',function() {
+    $(this).each(function() {
+      var image = $(this).attr('data-img');
+      $('.change-overview').attr("src", image);
+    });
+  });
+
+  // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 });
