88436c9ffaf9ac2b0a972439a88003a6ece16910
Steven Jerez <tetraman4arms@gmail.com>
2018-01-16 22:52:45 -0400

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('.hero-slider').slick({
     dots: true,
+    autoplay: true,
     prevArrow: '<div class="custom-prev"><i class="fas fa-chevron-left fa-lg"></i></div>',
     nextArrow: '<div class="custom-next"><i class="fas fa-chevron-right fa-lg"></i></div>'
   });
